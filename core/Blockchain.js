5094ada8b126fef6fff6d743be55c5b593719cf0
Yash Rane <yash.tushar13@gmail.com>
2026-01-29 04:04:36 -0800

    added validity function , checked for tempering

--- core/Blockchain.js
+++ core/Blockchain.js
@@ -23,12 +23,32 @@ class Blockchain
         this.chain.push(newBlock);
 
     }
+    isValidBlock()
+    {
+        for(let i =0 ; i<this.chain.length;i++)
+        {
+            let currentBlock = this.chain[i];
+            let previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash != currentBlock.calculateHash())
+            {
+                return false;
+            }
+            if(currentBlock.prevHash != previousBlock.hash )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
 
 let YERcoin = new Blockchain();
 YERcoin.addBlock(new Block(1 ,"02/01/2026" , {amount: 4}));
 YERcoin.addBlock(new Block(2 ,"03/01/2026" , {amount: 10}));
+
+// console.log("Is the block chain valid?" + YERcoin.isValidBlock());
 
 console.log(JSON.stringify(YERcoin , null , 4));
 export default Blockchain;
